f9331c4bae9487754be1e0662d8239d3619d1eb4
SeongjuneKim <bini060315@gmail.com>
2026-07-09 15:30:12 +0900

    AddMemory

--- Assets/Script/DebugManager.cs
+++ Assets/Script/DebugManager.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+using TMPro;
 
 public class DebugManager : MonoBehaviour
 {
     public GameObject debugPanel;
-    public Text debugText;
+    public GameObject debugPanelText;
+    public TextMeshProUGUI debugText;
+    public GameObject buttonGroup;
 
-    private int currentMode = 0;
+    bool debugMode = false;
 
     void Start()
     {
@@ -17,118 +20,52 @@ public class DebugManager : MonoBehaviour
 
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.P))
         {
             bool isActive = !debugPanel.activeSelf;
             debugPanel.SetActive(isActive);
 
+
             if (isActive)
             {
-                debugText.text = "버튼을 눌러 정보를 확인하세요.";
-                currentMode = 0;
+                debugMode = false;
+                buttonGroup.SetActive(true);         
+                debugText.gameObject.SetActive(false); 
+                debugPanelText.SetActive(true);
             }
         }
 
-        if (debugPanel == null || !debugPanel.activeSelf) return;
 
-        UpdateDebugText();
+        if (debugMode == true)
+        {
+            UpdateDebugText();
+        }
     }
 
     void UpdateDebugText()
     {
         VirusLaser laser = FindFirstObjectByType<VirusLaser>();
-        SwitchController[] switches = FindObjectsByType<SwitchController>(FindObjectsSortMode.None);
 
-        string resultText = "===== [디버그 모드] =====\n\n";
+        string resultText = "[노드 연결 상태]\n\n시작";
 
-        if (currentMode == 1)
+        List<string> hitObjects = GetLaserHitObjects(laser);
+
+        for (int i = 0; i < hitObjects.Count; i++)
         {
-            if (laser != null)
-            {
-                resultText += "[노드 연결 상태]\n시작";
-
-                List<string> hitObjects = GetLaserHitObjects(laser);
-
-                for (int i = 0; i < hitObjects.Count; i++)
-                {
-                    resultText += " -> " + hitObjects[i];
-                }
-
-                if (hitObjects.Count == 0)
-                {
-                    resultText += " -> 충돌 없음";
-                }
-            }
-            else
-            {
-                resultText += "맵에서 레이저를 찾을 수 없습니다.";
-            }
-        }
-        else if (currentMode == 2) // 2번 버튼: 현재 스위치 상태
-        {
-            if (switches != null && switches.Length > 0)
-            {
-                for (int i = 0; i < switches.Length; i++)
-                {
-                    SwitchController sw = switches[i];
-                    string stateStr = "";
-
-                    if (sw.isRed)
-                    {
-                        stateStr = "빨강 (RED)";
-                    }
-                    else
-                    {
-                        stateStr = "파랑 (BLUE)";
-                    }
-
-                    resultText += "[스위치 " + sw.gameObject.name + "] 상태: " + stateStr + "\n";
-                }
-            }
-            else
-            {
-                resultText += "맵에 배치된 스위치가 없습니다.";
-            }
-        }
-        else if (currentMode == 3) // 3번 버튼: 레이저 기본 수치 및 메모리 상태
-        {
-            if (laser != null)
-            {
-                // 소수점 떼고 정수로 각도 저장
-                float angle = Mathf.Round(laser.transform.eulerAngles.z);
-
-                resultText += "[레이저 정보]\n";
-                resultText += "현재 각도: " + angle.ToString() + "도\n";
-                resultText += "남은 반사 횟수: " + laser.maxBounce.ToString() + "회\n\n";
-            }
-            else
-            {
-                resultText += "레이저 정보 없음\n\n";
-            }
-
-            // GameManager가 있다면 메모리 정보도 추가 (구현된 GameManager 상태에 맞게 수정 가능)
-            if (GameManager.Instance != null)
-            {
-                resultText += "[메모리 상태]\n";
-                resultText += "게임 매니저 정상 연결됨\n";
-                // 예시: resultText += "현재 메모리: " + GameManager.Instance.memoryCount.ToString();
-            }
-        }
-        else
-        {
-            resultText += "아래 버튼 중 하나를 선택해주세요.";
+            resultText += " -> " + hitObjects[i];
         }
 
-        // 최종적으로 조합된 텍스트를 화면에 출력 ($, {} 사용 안 함)
+        if (hitObjects.Count == 0)
+        {
+            resultText += " -> 충돌 없음";
+        }
+
         debugText.text = resultText;
     }
 
-    // 선에 닿은 물체 가져오는 함수
     List<string> GetLaserHitObjects(VirusLaser laser)
     {
         List<string> objects = new List<string>();
-        if (laser == null) return objects;
 
         CheckLine(laser.lineRenderer1, objects, laser.objectLayer);
         CheckLine(laser.lineRenderer2, objects, laser.objectLayer);
@@ -145,7 +82,12 @@ public class DebugManager : MonoBehaviour
 
         for (int i = 0; i < points.Length - 1; i++)
         {
-            RaycastHit2D hit = Physics2D.Linecast(points[i], points[i + 1], layer);
+            Vector3 dir = (points[i + 1] - points[i]).normalized;
+            
+            Vector3 extendedEndPoint = points[i + 1] + dir * 0.05f;
+
+            RaycastHit2D hit = Physics2D.Linecast(points[i], extendedEndPoint, layer);
+
             if (hit.collider != null)
             {
                 if (!nameList.Contains(hit.collider.name))
@@ -156,37 +98,18 @@ public class DebugManager : MonoBehaviour
         }
     }
 
-    // ==========================================
-    // UI 버튼에 연결할 함수들입니다.
-    // ==========================================
-
-    public void OnClickPathButton()
+    public void DebugButton()
     {
-        currentMode = 1;
+        debugMode = true;
+        buttonGroup.SetActive(false);
+        debugText.gameObject.SetActive(true);
+        debugPanelText.SetActive(false);
     }
 
-    public void OnClickSwitchButton()
+    public void SkipLevel()
     {
-        currentMode = 2;
-    }
+        GameManager.Instance.NextStage(); 
 
-    public void OnClickInfoButton()
-    {
-        currentMode = 3;
-    }
-
-    // 레벨 스킵 버튼 기능
-    public void ForceSkipLevel()
-    {
-        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-
-        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
-        {
-            SceneManager.LoadScene(nextSceneIndex);
-        }
-        else
-        {
-            SceneManager.LoadScene(0);
-        }
+        debugPanel.SetActive(false);
     }
 }
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -110,6 +110,7 @@ public class GameManager : MonoBehaviour
     {
         if (currentMapInstance != null)
         {
+            currentMapInstance.SetActive(false);
             Destroy(currentMapInstance);
         }
 
